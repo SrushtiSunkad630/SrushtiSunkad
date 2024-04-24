7bc55fac44991e4952edf0bd032ea6cfe4006084
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-25 00:53:22 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,10 @@
+//print two character P and F
+#include<stdio.h>
+void main()
+{
+char a,b;
+printf("enter a values for a and b");
+scanf("%c %c", &a,&b);
+printf("a= %c,b= %c,\n",a,b);}
+
+OUTPUT: a=P,b=F
