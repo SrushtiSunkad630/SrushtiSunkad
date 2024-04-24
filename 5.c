db88e2668c553fb702acc8b8b18b7bc6cd7a1f62
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-25 00:58:33 +0530

    Create 5.c

---
+++ 5.c
@@ -0,0 +1,13 @@
+// take hours and minutes and calculate the total minutes
+#include<stdio.h>
+void main()
+{
+int H,M,TM;
+printf("enter a values for H,T");
+scanf("%d%d",&H,&M);
+TM=(H*60)+M;
+printf("Total minutes= %d\n",TM);
+}
+
+OUTPUT: H=2,M=20
+        TM=140
