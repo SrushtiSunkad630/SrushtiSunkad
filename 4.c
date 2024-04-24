3f7303c70b0438040bcca9f1ba81b26588db674a
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-25 00:55:38 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,13 @@
+// Find the area of circle
+#include<stdio.h>
+#define PI 3.14
+void main()
+{
+int r;
+float area;
+printf("Enter a value for r");
+scanf("%d", &r);
+area=PI*r*r;
+printf("area is= %f\n",area);}
+
+OUTPUT: r=20, area=1256.0000
