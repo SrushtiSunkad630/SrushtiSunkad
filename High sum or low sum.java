a2b7f3eb3badab98aec3ec57b18848d59c881f7d
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-26 20:27:04 +0530

    Create High sum or low sum.java

---
+++ High sum or low sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        long y=scn.nextInt();
+        if (x+y>=100)
+        System.out.println("High Sum");
+        else
+        System.out.println("Low Sum");
+
+    }
+}
