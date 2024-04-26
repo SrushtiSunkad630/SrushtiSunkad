777acf9cff93288019e3a116d0ace8b5692a3402
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-26 20:22:53 +0530

    Create Even or not.java

---
+++ Even or not.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int x=scn.nextInt();
+        if (x%2==0)
+        System.out.println("true");
+        else
+        System.out.println("false");
+    }
+}
