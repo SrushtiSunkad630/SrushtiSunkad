4eaa8401f0951918cc13358566dec34dec37c55d
Srushti Sunkad <167685577+SrushtiSunkad@users.noreply.github.com>
2024-04-26 20:25:23 +0530

    Create Grace marks.java

---
+++ Grace marks.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        int m=scn.nextInt();
+        if (m<33)
+        System.out.println(m+4);
+        else
+        System.out.println(m+0);
+    }
+}
